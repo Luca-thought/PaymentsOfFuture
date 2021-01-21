a24b424a0c1d22807f82647cd9f63a63da8823d0
Luca-thought <69170963+Luca-thought@users.noreply.github.com>
2021-01-21 16:46:51 +0100

    add icon

--- src/app/pages/your-cashback/your-cashback.component.ts
+++ src/app/pages/your-cashback/your-cashback.component.ts
@@ -12,27 +12,105 @@ export class YourCashback {
 
   listObjectPrefer = [{
     title : 'Transactions List',
-    icon : 'payment',
-    order: 2
+    icon : 'assets/abercrombie.png',
+    percentual: '10%',
+    expired : '2 days ago',
+    order: 1,
+    added: false
   },
   {
     title : 'Balance detail',
-    icon : 'euro_symbol',
-    order: 1
+    icon : 'assets/Airbnb.png',
+    percentual: '5%',
+    expired : '5 days ago',
+    order: 2,
+    added: false
   },
   {
     title : 'New Transfer',
-    icon : 'payment',
-    order: 3
+    icon : 'assets/footlocker.png',
+    percentual: '-',
+    expired : 'Not avaiable',
+    order: 3,
+    added: false
   }];
 
- constructor(
-    private router: Router) {
+  listObjectAvaiable = [{
+    title : 'Transactions List',
+    icon : 'assets/abercrombie.png',
+    percentual: '10%',
+    expired : '2 days ago',
+    order: 1,
+    added: false
+  },
+  {
+    title : 'Balance detail',
+    icon : 'assets/Airbnb.png',
+    percentual: '5%',
+    expired : '5 days ago',
+    order: 5,
+    added: false
+  },
+  {
+    title : 'New Transfer',
+    icon : 'assets/colemar.png',
+    percentual: '3',
+    expired : '5 days ago',
+    order: 3,
+    added: false
+  },
+  {
+    title : 'Transactions List',
+    icon : 'assets/IHG.png',
+    percentual: '3%',
+    expired : '20 days ago',
+    order: 2,
+    added: false
+  },
+  {
+    title : 'Balance detail',
+    icon : 'assets/coop.png',
+    percentual: '5%',
+    expired : '30 days ago',
+    order: 4,
+    added: false
+  },
+  {
+    title : 'New Transfer',
+    icon : 'assets/terranova.png',
+    percentual: '2%',
+    expired : '60 days ago',
+    order: 6,
+    added: false
+  }];
+
+  listObjectExpired = [];
+  listObjectSelected =[];
+
+ constructor() {
   }
 
-  goTo(elem){
-    this.router.navigate([elem.url]);
+  added(elem){
+    elem.added = true;
+    this.listObjectSelected.push(elem);
+    for(let i of this.listObjectPrefer){
+      if(i.icon === elem.icon){
+        i.added = true;
+      }
+    }
+  }
 
+  remove(elem){
+    elem.added = false;
+    this.listObjectSelected.forEach((element,index)=>{
+      if(element.added === false) {this.listObjectSelected.splice(index,1)};
+   });
+
+    for(let i of this.listObjectPrefer){
+      if(i.icon === elem.icon){
+        i.added = false;
+      }
+    }
   }
 
 }
